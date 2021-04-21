2b81bea897c361cc0622fcedf6d2d2cd749fa4c6
Lucia Seggiaro <luciaseggiaro@gmail.com>
2021-04-20 21:13:37 -0300

    added author endpoint limiter

--- src/routers/books.router.mjs
+++ src/routers/books.router.mjs
@@ -1,13 +1,23 @@
 import { Router } from 'express';
 import { data } from '../database/db.mjs';
 import { middlewarePaginaPrivada } from '../middlewares/privacy.middleware.mjs';
+import rateLimit from 'express-rate-limit';
 
-
+const limiter = rateLimit({
+  windowMs: 60 * 1000, // una hora
+  max: 5 // limit each IP to 100 requests per windowMs
+});
 
 function miAtrapador(request, response) {
   console.log('==>', request.jajaja);
   response.json(data);
 };
+
+function miAtrapadorAutor(request, response) {
+  console.log('==>', request.jajaja);
+  response.json(data);
+};
+
 
 const creaLibro = (request, response) => {
   data.push(
@@ -20,5 +30,6 @@ export function getRouter() {
   const router = new Router();
   router.get('/', miAtrapador);
   router.post('/', middlewarePaginaPrivada, creaLibro);
+  router.get('/autor', limiter, miAtrapadorAutor);
   return router;
 }
